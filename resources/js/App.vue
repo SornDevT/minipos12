4275fe3fe437a6828ed02beff6ceb32ef906ed26
SornDevT <sorndev.teach@gmail.com>
2023-10-18 19:50:54 +0700

    first commit-upload

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vue App</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos12App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
